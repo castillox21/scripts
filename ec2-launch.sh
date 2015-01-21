3214236367422738477bec406b7f90b25d545d79
Carlos Castillo <castillox21@gmail.com>
2015-01-21 13:45:22 -0500

    Updated ec2-launch.sh

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -4,24 +4,36 @@
 
 clear
 
-echo "Enter an instance type (t2.micro): "
+echo -e "Enter an instance type (t2.micro): \c"
 read instance
+if [ -z "$instance" ]; then
+   instance=t2.micro
+fi  
 echo "Your instance type is: $instance"
 
-echo "Enter an image id (a8d369c0): "
+echo -e "Enter an image id (a8d369c0): \c"
 read imageid
+if [ -z "$imageid" ]; then
+   imageid=a8d369c0
+fi   
 echo "Your AMI ID is: $imageid"
 
-#Ran to show keypairs
-aws ec2 describe-key-pairs
+aws ec2 describe-key-pairs #Command to show keypairs. 
 
 echo " "
-echo "Enter one of your available keypairs (name): "
+echo -e "Enter one of your available keypairs (name): \c"
 read keypair
+if [ -z "$keypair" ]; then
+   echo "Aborting, You must enter a valid keypair!"
+   exit 0
+fi
 echo "Your selected keypair is: $keypair"
 
-echo "Enter a security group (default): "
+echo -e "Enter a security group (default): \c"
 read  group
+if [ -z "$group" ]; then
+   group=default
+fi
 echo "Your selected group is: $group" 
 
 aws ec2 run-instances --image-id ami-$imageid --count 1 --instance-type $instance --key-name $keypair --security-groups $group
