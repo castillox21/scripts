6c0c0b382f3880729cf4bd9edf32eab6cb9ad97c
Henry Franco <henry@henryfranco.com>
2015-01-23 16:12:29 +0000

    Pushing my changes

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,31 +1,39 @@
-#!/bin/bash
+#! /bin/bash
 
 #This script launches a new ec2 instance using the input values.
 
 clear
 
-# Ask for instance type
-echo "Enter an instance type (t2.micro): "
+echo -e "Enter an instance type (t2.micro): \c"
 read instance
+if [ -z "$instance" ]; then
+   instance=t2.micro
+fi  
 echo "Your instance type is: $instance"
 
-# Ask for image id
-echo "Enter an image id (a8d369c0): "
+echo -e "Enter an image id (a8d369c0): \c"
 read imageid
+if [ -z "$imageid" ]; then
+   imageid=a8d369c0
+fi   
 echo "Your AMI ID is: $imageid"
 
-# Show keypairs
-aws ec2 describe-key-pairs
+aws ec2 describe-key-pairs #Command to show keypairs. 
 
-# Ask for key pair
 echo " "
-echo "Enter one of your available keypairs (name): "
+echo -e "Enter one of your available keypairs (name): \c"
 read keypair
+if [ -z "$keypair" ]; then
+   echo "Aborting, You must enter a valid keypair!"
+   exit 0
+fi
 echo "Your selected keypair is: $keypair"
 
-# Ask for security group
-echo "Enter a security group (default): "
+echo -e "Enter a security group (default): \c"
 read  group
+if [ -z "$group" ]; then
+   group=default
+fi
 echo "Your selected group is: $group" 
 
 # Remove any previous files
